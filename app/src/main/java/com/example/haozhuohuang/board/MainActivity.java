77e5d4099349c5b769be35556f3ad24c145e1513
Haozhuo <vichuanghaozhuo@outlook.com>
2017-03-31 14:23:02 -0700

    finish

--- app/src/main/java/com/example/haozhuohuang/board/MainActivity.java
+++ app/src/main/java/com/example/haozhuohuang/board/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.haozhuohuang.board;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
+import android.widget.GridLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     //0 yellow; 1 red
     int activePlayer = 0;
+    boolean isGameActive = true;
+    int gridOccupied = 0;
 
     //2 means untouched
     int [] gameState = {2,2,2,2,2,2,2,2,2};
@@ -19,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         //get which chess is tapped
         int tappedCounter = Integer.parseInt(counter.getTag().toString());
         //if the grid is un-tapped
-        if(gameState[tappedCounter]==2){
+        if(gameState[tappedCounter]==2 && isGameActive){
             counter.setTranslationY(-1000f);
             //change the gameState to current active player
             gameState[tappedCounter] = activePlayer;
@@ -34,15 +40,53 @@ public class MainActivity extends AppCompatActivity {
                 counter.setImageResource(R.drawable.red);
                 activePlayer = 0;
             }
+
+            gridOccupied++;
             //animation
-            counter.animate().translationYBy(1000f).setDuration(300);
+            counter.animate().translationYBy(1000f).setDuration(100);
+
+            if(gridOccupied==9){
+                System.out.println("Here");
+                isGameActive = false;
+
+                TextView winMessage = (TextView) findViewById(R.id.playAgainText);
+                winMessage.setText("It's a draw!");
+                LinearLayout layout = (LinearLayout) findViewById(R.id.playAgainLayout);
+                layout.setVisibility(View.VISIBLE);
+                return;
+            }
 
             for(int [] winPos:winningPos){
                 if(gameState[winPos[0]] != 2 && gameState[winPos[0]]==gameState[winPos[1]]
                         &&gameState[winPos[1]]==gameState[winPos[2]]){
-                    System.out.println(gameState[winPos[0]]);
+                    String winner = "red";
+                    if(gameState[winPos[0]]==0)
+                        winner = "yellow";
+                    //someone has win a game
+                    TextView winMessage = (TextView) findViewById(R.id.playAgainText);
+                    winMessage.setText(winner+" win the game");
+                    LinearLayout layout = (LinearLayout) findViewById(R.id.playAgainLayout);
+                    layout.setVisibility(View.VISIBLE);
+                    isGameActive = false;
                 }
             }
+        }
+    }
+
+    public void playAgain(View view){
+        LinearLayout layout = (LinearLayout) findViewById(R.id.playAgainLayout);
+        layout.setVisibility(View.INVISIBLE);
+        isGameActive = true;
+        gridOccupied = 0;
+
+        activePlayer = 0;
+        for(int i = 0; i < gameState.length; i++)
+            gameState[i] = 2;
+
+        GridLayout grid = (GridLayout) findViewById(R.id.grid);
+
+        for(int i = 0; i < grid.getChildCount(); i++){
+            ((ImageView) grid.getChildAt(i)).setImageResource(0);
         }
     }
 
